92c79573aaf8b27e399945efc56392f70a84a23f
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2020-09-13 19:25:17 +0200

    Added API functionality for saving files

--- apiRouter.js
+++ apiRouter.js
@@ -1,8 +1,8 @@
 /**
  * Emulates Weltmeister PHP backend
  */
+const fs = require('fs');
 const path = require('path');
-
 const glob = require('glob');
 const express = require('express');
 const router = express.Router();
@@ -34,39 +34,70 @@ router.get('/glob.php', async (req, res) => {
 });
 
 router.get('/browse.php', (req, res) => {
-    const {query} = req;
+    const dirParam = req.param('dir') || '';
+    const type = req.param('type');
 
-    if (query.hasOwnProperty('dir') && query.hasOwnProperty('type')) {
-        const searchDir = path.join(__fileRoot, query.dir);
+    const types = {
+        scripts: ['.js'],
+        images: ['.png', '.gif', '.jpg', '.jpeg']
+    };
 
-        let type = '';
+    const result = {
+        parent: dirParam.length > 0 ? dirParam.substring(0, dirParam.lastIndexOf('/')) : false,
+        dirs: [],
+        files: []
+    };
 
-        switch (query.type) {
-            case 'scripts':
-                type = '*.js';
-                break;
-            case 'images':
-                type = '*.{png,gif,jpg,jpeg}';
-                break;
+    const filter = type && types.hasOwnProperty(type) ? types[type] : false;
+
+    const searchDir = path.join(__fileRoot, dirParam);
+
+    fs.readdir(searchDir, (err, files) => {
+        if (err) console.error(err);
+
+        for (const i in files) {
+            const stats = fs.statSync(path.join(searchDir, files[i]));
+
+            if (stats.isDirectory()) {
+                result.dirs.push(path.join(dirParam, files[i]));
+            } else if (stats.isFile()) {
+                if (filter) {
+                    if (filter.indexOf(path.extname(files[i])) !== -1) {
+                        result.files.push(path.join(dirParam, files[i]));
+                    }
+                } else {
+                    result.files.push(path.join(dirParam, files[i]));
+                }
+            }
         }
 
-        const dirs = glob.sync(`${searchDir}/*/`);
-        const files = glob.sync(`${searchDir}/${type}`);
-
-        res.send({
-            parent: query.dir.length > 0 ? query.dir.slice(0, query.dir.lastIndexOf('/')) : false,
-            dirs: dirs.map(dir => dir.replace(__fileRoot, '').replace(/\/$/, '')),
-            files: files.map(file => file.replace(__fileRoot, ''))
-        });
-    } else {
-        res.send();
-    }
+        res.send(result);
+    });
 });
 
 router.post('/save.php', (req, res) => {
-    console.log(req);
+    const leveldata = req.body.data;
+    const levelpath = req.body.path;
 
-    res.send();
+    if (leveldata && levelpath && /\.js$/.test(levelpath)) {
+        fs.writeFile(path.join(__fileRoot, levelpath), leveldata, err => {
+            if (err) {
+                res.send({
+                    error: 2,
+                    msg: err
+                });
+            } else {
+                res.send({
+                    error: 0
+                });
+            }
+        });
+    } else {
+        res.send({
+            error: 1,
+            msg: 'No data or path specified'
+        });
+    }
 });
 
 module.exports = router;
